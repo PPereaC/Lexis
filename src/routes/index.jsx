2aec38b22547cddb27c3bc67d25fa2e2ca9f55ca
Pablo Perea Campos <pablopereac02@gmail.com>
2026-02-16 16:36:02 +0100

    feat(routes): añadida ruta para la página de últimos lanzamientos

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import MainLayout from '../layouts/MainLayout'
 import NewNTrendingPage from '../pages/NewNTrendingPage'
+import LastReleasesPage from '../pages/LastReleasesPage'
 
 import '../App.css'
 
@@ -8,6 +9,7 @@ function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout><NewNTrendingPage /></MainLayout>} />
+      <Route path="/ultimos-lanzamientos" element={<MainLayout><LastReleasesPage /></MainLayout>} />
     </Routes>
   )
 }
