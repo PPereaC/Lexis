692453bc17a1377f04344ec46f1e9ac07ee727e1
Pablo <pereacampospablo@gmail.com>
2026-02-22 12:44:23 +0100

    refactor(routes): renombrado archivo NewNTrendingPage a TrendingPage

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import MainLayout from '../layouts/MainLayout'
-import NewNTrendingPage from '../pages/NewNTrendingPage'
+import NewNTrendingPage from '../pages/TrendingPage'
 import LastReleasesPage from '../pages/LastReleasesPage'
 import VideoGameDetailsPage from '../pages/VideoGameDetailsPage'
 
