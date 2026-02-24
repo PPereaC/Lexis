23d1c862a96283a46f99755bcab340d616eb31a6
Pablo <pereacampospablo@gmail.com>
2026-02-24 13:01:37 +0100

    feat(routes): añadido sistema de autenticación con rutas protegidas, públicas y de administrador

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from '../contexts/AuthContext.jsx'
+import { ProtectedRoute, AdminRoute, PublicRoute } from '../components/ProtectedRoute.jsx'
 import MainLayout from '../layouts/MainLayout'
 import NewNTrendingPage from '../pages/TrendingPage'
 import LastReleasesPage from '../pages/LastReleasesPage'
@@ -7,20 +9,39 @@ import SearchResultsPage from '../pages/SearchResultsPage'
 import NextReleasesPage from '../pages/NextReleasesPage'
 import GenreResultsPage from '../pages/GenreResultsPage'
 import TopGamesPage from '../pages/TopGamesPage'
+import LoginPage from '../pages/LoginPage.jsx'
+import RegisterPage from '../pages/RegisterPage.jsx'
+import AdminPage from '../pages/AdminPage.jsx'
+import ProfilePage from '../pages/ProfilePage.jsx'
 import '../App.css'
 
 function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/tendencias" replace />} />
-      <Route path="/tendencias" element={<MainLayout><NewNTrendingPage /></MainLayout>} />
-      <Route path="/novedades" element={<MainLayout><LastReleasesPage /></MainLayout>} />
-      <Route path="/juego/:id" element={<MainLayout><VideoGameDetailsPage /></MainLayout>} />
-      <Route path="/buscar" element={<MainLayout><SearchResultsPage /></MainLayout>} />
-      <Route path="/proximamente" element={<MainLayout><NextReleasesPage /></MainLayout>} />
-      <Route path="/genero/:genero" element={<MainLayout><GenreResultsPage /></MainLayout>} />
-      <Route path="/top" element={<MainLayout><TopGamesPage /></MainLayout>} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        {/* Redirigir a login si no está autenticado */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        
+        {/* Todas las rutas de juegos - Protegidas (requieren login) */}
+        <Route path="/tendencias" element={<ProtectedRoute><MainLayout><NewNTrendingPage /></MainLayout></ProtectedRoute>} />
+        <Route path="/novedades" element={<ProtectedRoute><MainLayout><LastReleasesPage /></MainLayout></ProtectedRoute>} />
+        <Route path="/juego/:id" element={<ProtectedRoute><MainLayout><VideoGameDetailsPage /></MainLayout></ProtectedRoute>} />
+        <Route path="/buscar" element={<ProtectedRoute><MainLayout><SearchResultsPage /></MainLayout></ProtectedRoute>} />
+        <Route path="/proximamente" element={<ProtectedRoute><MainLayout><NextReleasesPage /></MainLayout></ProtectedRoute>} />
+        <Route path="/genero/:genero" element={<ProtectedRoute><MainLayout><GenreResultsPage /></MainLayout></ProtectedRoute>} />
+        <Route path="/top" element={<ProtectedRoute><MainLayout><TopGamesPage /></MainLayout></ProtectedRoute>} />
+        
+        {/* Rutas de autenticación - Públicas */}
+        <Route path="/login" element={<PublicRoute><LoginPage /></PublicRoute>} />
+        <Route path="/registro" element={<PublicRoute><RegisterPage /></PublicRoute>} />
+        
+        {/* Ruta de administración - Solo admin */}
+        <Route path="/admin" element={<AdminRoute><AdminPage /></AdminRoute>} />
+        
+        {/* Ruta de perfil - Solo usuarios autenticados */}
+        <Route path="/perfil" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
+      </Routes>
+    </AuthProvider>
   )
 }
 
